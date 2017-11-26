bab4a3e6e605d72dd4b55d3688a62b75f6eda811
wmc911 <wangmingcheng2016@gmail.com>
2017-11-26 12:33:55 +0800

    获取服务器端盗贼告警

--- src/pages/thief-warning/thief-warning.ts
+++ src/pages/thief-warning/thief-warning.ts
@@ -4,6 +4,7 @@ import { ThiefWarningHistoryPage } from '../thief-warning-history/thief-warning-
 import { ThiefFakeKeyPage } from '../thief-fake-key/thief-fake-key';
 import { ThiefDemolitionLockCorePage } from '../thief-demolition-lock-core/thief-demolition-lock-core'
 import { ThiefPryDoorPage } from '../thief-pry-door/thief-pry-door'
+import { Api } from '../../providers/api/api';
 
 /**
  * Generated class for the ThiefWarningPage page.
@@ -21,28 +22,11 @@ export class ThiefWarningPage {
   thiefWarningItems: any[];
   profilePic: any
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api) {
     this.profilePic = "assets/img/warn.png";
-    this.thiefWarningItems = [
-      {
-        "thiefWarningId": 1,
-        "name": "盗贼假钥匙开锁报警",
-        "time": "2017年10月31日 下午10:47",
-        "hasSignal": true
-      },
-      {
-        "thiefWarningId": 2,
-        "name": "盗贼拆锁芯报警",
-        "time": "2017年10月31日 下午10:47",
-        "hasSignal": true
-      },
-      {
-        "thiefWarningId": 3,
-        "name": "盗贼撬门报警",
-        "time": "2017年10月31日 下午10:47",
-        "hasSignal": false
-      }
-    ];
+
+    this.getWarningLog();
+
   }
 
   ionViewDidLoad() {
@@ -61,6 +45,49 @@ export class ThiefWarningPage {
       this.navCtrl.push(ThiefDemolitionLockCorePage);
     } else if (thiefWarningId == 3) {
       this.navCtrl.push(ThiefPryDoorPage);
+    }
+  }
+
+  getWarningLog() {
+    let seq = this.api.authGet('main/getWarningLog').share();
+
+    seq.subscribe((resp: any) => {
+      // If the API returned a successful response, mark the user as logged in
+      var res = resp.json();
+      console.log(res)
+      if (res['status'] == true) {
+        this.convert2ThiefWarning(res['data']);
+        console.log(res['data'])
+        console.log(this.thiefWarningItems)
+      } else {
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return seq;
+  }
+
+  convert2ThiefWarning(res: Array<any>) {
+    this.thiefWarningItems = [];
+    for (var i = 0; i < res.length && i < 3; i++) {
+      var item = {};
+      var warning_log = res[i]['warning_log']
+      item['thiefWarningId'] = warning_log
+      item['time'] = res[i]['create_time']
+      item['hasSignal'] = true
+      switch (warning_log) {
+        case 1:
+          item['name'] = "盗贼假钥匙开锁报警";
+          break;
+        case 2:
+          item['name'] = "盗贼拆锁芯报警";
+          break;
+        case 3:
+          item['name'] = "盗贼撬门报警";
+          break;
+      }
+      this.thiefWarningItems.push(item)
+
     }
   }
 }
