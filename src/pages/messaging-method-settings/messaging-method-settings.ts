165b2706688624e16fe9ff63cf5d3daf956e161d
wmc911 <wangmingcheng2016@gmail.com>
2017-12-11 00:15:23 +0800

    进出门消息传递方式界面

--- src/pages/messaging-method-settings/messaging-method-settings.ts
+++ src/pages/messaging-method-settings/messaging-method-settings.ts
@@ -14,12 +14,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'messaging-method-settings.html',
 })
 export class MessagingMethodSettingsPage {
+  methodItem: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.methodItem = 1
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MessagingMethodSettingsPage');
   }
 
+  save() {
+    console.log(this.methodItem)
+  }
 }
