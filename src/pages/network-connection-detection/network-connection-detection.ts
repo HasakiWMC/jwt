3f4f04ee7167fdec4a51ec3053a551f68021d40b
wmc911 <wangmingcheng2016@gmail.com>
2017-12-10 18:59:53 +0800

    网络连接检测用Loading组件

--- src/pages/network-connection-detection/network-connection-detection.ts
+++ src/pages/network-connection-detection/network-connection-detection.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ViewChild, QueryList, ElementRef, AfterViewInit, Renderer, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { ViewChild, QueryList, Renderer, OnInit } from '@angular/core';
 
 @IonicPage()
 @Component({
@@ -13,18 +13,26 @@ export class NetworkConnectionDetectionPage {
 
   isStartDetection: boolean;
   isOverDetection: boolean;
-  curRate: number;
-  preRate: number;
-  dectectionTime: any;
+  successRate: number;
+  dectectionTime: number;
+  loadingInstance: any;
 
-  @ViewChild('myProgressBar')
-  myProgressBar: ElementRef;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private elementRef: ElementRef, private renderer: Renderer) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController
+  ) {
     this.isStartDetection = false;
     this.isOverDetection = false;
-    this.curRate = 0;
-    this.preRate = 0;
+  }
+
+  presentLoadingDefault() {
+    this.loadingInstance = this.loadingCtrl.create({
+      content: '正在进行网络检测...',
+      duration: 10000
+    });
+
+    this.loadingInstance.present();
   }
 
   ionViewDidLoad() {
@@ -32,40 +40,23 @@ export class NetworkConnectionDetectionPage {
   }
 
   startDetection() {
-    this.curRate = 0;
+    this.presentLoadingDefault()
     this.isOverDetection = false;
     this.isStartDetection = true;
     this.dectectionTime = setInterval(() => {
-      this.getdetectionRate();
-    }, 2000);
-
+      this.getdetectionResult();
+    }, 1000);
   }
 
-  public getdetectionRate() {
-    if (this.curRate < 100) {
-      this.preRate = this.curRate;
-      this.curRate = this.curRate + 10;
-      var delta = this.curRate - this.preRate;
-      this.renderer.setElementStyle(this.myProgressBar.nativeElement, 'width', this.curRate + '%');
-    }
-    else {
+  public getdetectionResult() {
+    //todo
+    //向后台查询是否有成功率的值，如果查询到结束定时查找函数，并消除Loading框
+
+
+    if (true) {
+      this.loadingInstance.dismiss();
       clearInterval(this.dectectionTime)
-      this.isOverDetection = true;
     }
 
   }
-
-  closeDetection() {
-    clearInterval(this.dectectionTime)
-    this.isStartDetection = false;
-    this.curRate = 0
-  }
-  // ngAfterViewInit() {
-  //   // this.myProgressBar.nativeElement.style.width = '20%';
-  //   // this.renderer.setElementStyle(this.myProgressBar.nativeElement, 'width', '20%');
-  // }
-
-  // ngOnInit() {
-
-  // }
 }
