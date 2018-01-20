51bc3053e2cb709bffaa8669b45f7b5bf5e669f1
wmc911 <wangmingcheng2016@gmail.com>
2018-01-20 20:13:42 +0800

    现有锁具特征确认 post方法

--- src/pages/lock-feature-confirm/lock-feature-confirm.ts
+++ src/pages/lock-feature-confirm/lock-feature-confirm.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import {Api} from '../../providers/api/api';
 
 /**
  * Generated class for the LockFeatureConfirmPage page.
@@ -15,7 +16,8 @@ import { IonicPage, NavController, NavParams,ToastController } from 'ionic-angul
 })
 export class LockFeatureConfirmPage {
   lockFeatureItem: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public api: Api) {
     if (localStorage.getItem('lockFeatureItem') == null) {
       this.lockFeatureItem = 1
     }
@@ -29,14 +31,39 @@ export class LockFeatureConfirmPage {
   }
 
   save() {
-    console.log(this.lockFeatureItem)
-    localStorage.setItem('lockFeatureItem', this.lockFeatureItem)
-    let toast = this.toastCtrl.create({
-      message: '保存成功',
-      duration: 1000,
-      position: 'top'
-    });
-    toast.present();
+    console.log(this.lockFeatureItem);
+    localStorage.setItem('lockFeatureItem', this.lockFeatureItem);
+    this.setLockerFeature();
   }
+
+  setLockerFeature() {
+    let option = {
+      "feature_id": this.lockFeatureItem
+    };
+    let seq = this.api.authPost('device/lockerFeatureConfirm', option).share();
+    seq.subscribe((resp: any) => {
+      let res = resp.json();
+      console.log(res);
+      if (res['status'] == true) {
+        let toast = this.toastCtrl.create({
+          message: '保存成功',
+          duration: 1000,
+          position: 'top'
+        });
+        toast.present();
+      } else {
+        let toast = this.toastCtrl.create({
+          message: '保存失败',
+          duration: 1000,
+          position: 'top'
+        });
+        toast.present();
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return seq;
+  }
+
 
 }
