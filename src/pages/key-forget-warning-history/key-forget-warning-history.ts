bd487bb55935f51a4e7f3942b265110f9f45b900
wmc911 <wangmingcheng2016@gmail.com>
2018-01-20 12:53:15 +0800

    钥匙出报警及查询历史

--- src/pages/key-forget-warning-history/key-forget-warning-history.ts
+++ src/pages/key-forget-warning-history/key-forget-warning-history.ts
@@ -14,56 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'key-forget-warning-history.html',
 })
 export class KeyForgetWarningHistoryPage {
-  keyForgetWarningItems: any[];
+  keyForgetList: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.keyForgetWarningItems = [
-      {
-        "thiefWarningId": 1,
-        "name": "盗贼假钥匙开锁报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 2,
-        "name": "盗贼拆锁芯报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 3,
-        "name": "盗贼撬门报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 4,
-        "name": "盗贼假钥匙开锁报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 5,
-        "name": "盗贼拆锁芯报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 6,
-        "name": "盗贼撬门报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 7,
-        "name": "盗贼假钥匙开锁报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 8,
-        "name": "盗贼拆锁芯报警",
-        "time": "2017年10月31日 下午10:47"
-      },
-      {
-        "thiefWarningId": 9,
-        "name": "盗贼撬门报警",
-        "time": "2017年10月31日 下午10:47"
-      }
-    ];
+    this.keyForgetList = navParams.get('keyForgetList');
   }
 
   ionViewDidLoad() {
--- src/pages/key-forget-warning/key-forget-warning.ts
+++ src/pages/key-forget-warning/key-forget-warning.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { KeyForgetWarningHistoryPage } from '../key-forget-warning-history/key-forget-warning-history';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {KeyForgetWarningHistoryPage} from '../key-forget-warning-history/key-forget-warning-history';
+import {Api} from '../../providers/api/api';
 
 /**
  * Generated class for the KeyForgetWarningPage page.
@@ -15,8 +16,11 @@ import { KeyForgetWarningHistoryPage } from '../key-forget-warning-history/key-f
   templateUrl: 'key-forget-warning.html',
 })
 export class KeyForgetWarningPage {
+  lastForgetTime: string;
+  keyForgetList: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api) {
+    this.getKeyForgetLog();
   }
 
   ionViewDidLoad() {
@@ -24,7 +28,42 @@ export class KeyForgetWarningPage {
   }
 
   queryKeyForgetWarningHistory() {
-    this.navCtrl.push(KeyForgetWarningHistoryPage);
+    this.navCtrl.push(KeyForgetWarningHistoryPage, {
+      keyForgetList: this.keyForgetList
+    });
+  }
+
+  getKeyForgetLog() {
+    let seq = this.api.authGet('main/forgetKey').share();
+
+    seq.subscribe((resp: any) => {
+      // If the API returned a successful response, mark the user as logged in
+      let res = resp.json();
+      console.log(res);
+      if (res['status'] == true) {
+        this.convert2KeyForgetWarning(res['data']);
+        console.log(res['data']);
+        console.log(this.keyForgetList);
+        console.log(this.lastForgetTime);
+      } else {
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return seq;
+  }
+
+  convert2KeyForgetWarning(res: Array<any>) {
+    this.lastForgetTime = "";
+    this.keyForgetList = [];
+    for (let i = 0; i < res.length; i++) {
+      let item = {};
+      item['time'] = res[i]['create_time'];
+      this.keyForgetList.push(item);
+    }
+    if (this.keyForgetList.length > 0) {
+      this.lastForgetTime = this.keyForgetList[this.keyForgetList.length - 1]["time"];
+    }
   }
 
 }
