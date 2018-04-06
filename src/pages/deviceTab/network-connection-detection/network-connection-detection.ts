4528a052f302a3c7540965fd7ca5a89c21cdeb9e
wmc911 <wangmingcheng2016@gmail.com>
2018-04-07 01:05:41 +0800

    网络检测完成

--- src/pages/deviceTab/network-connection-detection/network-connection-detection.ts
+++ src/pages/deviceTab/network-connection-detection/network-connection-detection.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
-import { ViewChild, QueryList, Renderer, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, LoadingController, ToastController} from 'ionic-angular';
+import {ViewChild, QueryList, Renderer, OnInit} from '@angular/core';
+import {Api} from "../../../providers/api/api";
 
 @IonicPage()
 @Component({
@@ -13,23 +14,29 @@ export class NetworkConnectionDetectionPage {
 
   isStartDetection: boolean;
   isOverDetection: boolean;
-  successRate: number;
-  dectectionTime: number;
+  successRate: any;
+  detectionTime: number;
   loadingInstance: any;
+  //loading框超时秒数
+  loadingTimeOut: number;
+  //尝试查询的次数
+  tryTime: number;
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public loadingCtrl: LoadingController
-  ) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public api: Api,
+              public toastCtrl: ToastController,
+              public loadingCtrl: LoadingController) {
     this.isStartDetection = false;
     this.isOverDetection = false;
+    this.loadingTimeOut = 10;
+    this.tryTime = 0;
   }
 
   presentLoadingDefault() {
     this.loadingInstance = this.loadingCtrl.create({
       content: '正在进行网络检测...',
-      duration: 10000
+      // duration: this.loadingTimeOut * 1000
     });
 
     this.loadingInstance.present();
@@ -40,25 +47,57 @@ export class NetworkConnectionDetectionPage {
   }
 
   startDetection() {
-    this.presentLoadingDefault()
-    this.isOverDetection = false;
-    this.isStartDetection = true;
-    this.dectectionTime = setInterval(() => {
-      this.getdetectionResult();
-    }, 1000);
+    let seq = this.api.authPost('device/networkDetection', {}).share();
+    seq.subscribe((resp: any) => {
+      let res = resp.json();
+      console.log(res);
+      if (res['status'] == true) {
+        this.tryTime = 0;
+        this.presentLoadingDefault();
+        this.isOverDetection = false;
+        this.isStartDetection = true;
+        this.detectionTime = setInterval(() => {
+          this.getDetectionResult();
+        }, 1000);
+      } else {
+        let toast = this.toastCtrl.create({
+          message: '启动网络检测失败',
+          duration: 1000,
+          position: 'top'
+        });
+        toast.present();
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return seq;
   }
 
-  public getdetectionResult() {
-    //todo
+  public getDetectionResult() {
     //向后台查询是否有成功率的值，如果查询到结束定时查找函数，并消除Loading框
+    let seq = this.api.authGet('device/networkQuery').share();
+    seq.subscribe((resp: any) => {
+      let res = resp.json();
+      console.log(res);
+      if (res['status'] == true) {
+        this.successRate = res['msg'];
+        this.loadingInstance.dismiss();
+        clearInterval(this.detectionTime);
+        this.isOverDetection = true
+      } else if (this.tryTime < this.loadingTimeOut) {
+        this.tryTime++;
+      } else {
+        this.loadingInstance.dismiss();
+        this.isOverDetection = true;
+        this.successRate = "网络不通";
+        clearInterval(this.detectionTime);
+      }
 
+    }, err => {
+      console.error('ERROR', err);
+    });
+    return seq;
 
-    if (true) {
-      this.successRate = 100
-      this.loadingInstance.dismiss();
-      clearInterval(this.dectectionTime)
-      this.isOverDetection = true
-    }
 
   }
 }
